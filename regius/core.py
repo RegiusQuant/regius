daeafaa5f00b6584dc22391f8589b864d31f26fb
RegiusQuant <315135833@qq.com>
2020-01-30 15:05:28 +0800

    Add Wide&Deep Dataset

--- regius/core.py
+++ regius/core.py
@@ -9,8 +9,12 @@
 from typing import Tuple, List, Dict, Optional
 
 import numpy as np
+from sklearn.utils import Bunch
 
 # torch相关引用
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 from torch import Tensor
+from torch.utils.data import Dataset, DataLoader
