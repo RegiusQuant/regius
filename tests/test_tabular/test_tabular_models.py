7e8a5f370701061003f07253ff78c2f59f56f73c
RegiusQuant <315135833@qq.com>
2020-01-30 13:26:20 +0800

    Tabular -> Table

--- tests/test_tabular/test_tabular_models.py
+++ tests/test_tabular/test_tabular_models.py
@@ -4,41 +4,33 @@ import sys
 
 sys.path.append(os.getcwd())
 
-import pytest
 import torch
 
-from regius.tabular.models import Wide, DeepDense
+from regius.table.models import Wide, DeepDense
+
 
 ############################################################
 # Wide模块测试
 ############################################################
-input_tensor_1 = torch.rand(4, 4)
-input_tensor_2 = torch.rand(10, 4)
-output_shape_1 = torch.Size([4, 1])
-output_shape_2 = torch.Size([10, 1])
-
-
-@pytest.mark.parametrize('input_tensor, output_shape',
-                         [(input_tensor_1, output_shape_1),
-                          (input_tensor_2, output_shape_2)])
-def test_wide(input_tensor, output_shape):
+def test_wide():
+    input_tensor = torch.rand(10, 4)
     model = Wide(in_dim=4, out_dim=1)
     output_tensor = model(input_tensor)
-    assert output_tensor.shape == output_shape
+    assert output_tensor.shape == torch.Size([10, 1])
 
 
 ############################################################
 # DeepDense模块测试
 ############################################################
-x_deepdense = torch.cat([torch.empty(10, 4).random_(10), torch.rand(10, 4)], 1)
-
-column_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-column_idx = {v: k for k, v in enumerate(column_names)}
-embed_input = [(u, i, j)
-               for u, i, j in zip(column_names[:4], [10] * 4, [5] * 4)]
-
-
 def test_deepdense():
+    input_tensor = torch.cat(
+        [torch.empty(10, 4).random_(10),
+         torch.rand(10, 4)], 1)
+    column_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    column_idx = {v: k for k, v in enumerate(column_names)}
+    embed_input = [(u, i, j)
+                   for u, i, j in zip(column_names[:4], [10] * 4, [5] * 4)]
+
     model = DeepDense(column_idx=column_idx,
                       hidden_nodes=[16, 8],
                       hidden_drop_ps=[0.2, 0.2],
@@ -46,5 +38,5 @@ def test_deepdense():
                       embed_input=embed_input,
                       embed_drop_p=0.5,
                       cont_cols=['e', 'f', 'g', 'h'])
-    output_tensor = model(x_deepdense)
+    output_tensor = model(input_tensor)
     assert output_tensor.shape == torch.Size([10, 8])
